d1623c4fe1825037748c47a9b2ea6de0061410bd
atos89 <atos_jam.1015@i.softbank.jp>
2018-02-18 03:09:39 +0900

    gulpのベースプロジェクトを作成

---
+++ gulp/config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const paths = {
+    src: {
+        root:   './',
+        html:   './src/html',
+        static: './src/static',
+        js:     './src/static/js',
+        stylus: './src/static/stylus'
+    },
+    dst: {
+        root:   './',
+        html:   './templates',
+        static: './static',
+        js:     './static/js',
+        css:    './static/css'
+    }
+};
+
+module.exports = {
+  paths: paths
+};
